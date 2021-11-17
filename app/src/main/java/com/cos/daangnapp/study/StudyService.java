37d1eda8d43fec4338c73b0e7306e0034ad5872e
jaedeok95 <dlwoejr700@gmail.com>
2021-11-18 01:29:52 +0900

    feat : study backbone

--- app/src/main/java/com/cos/daangnapp/study/StudyService.java
+++ app/src/main/java/com/cos/daangnapp/study/StudyService.java
@@ -7,15 +7,21 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
-public interface StudyListService {
+public interface StudyService {
     //사용자 분별위해 phoneNumber도 같이 보내주기!
-    @GET("study")
+    @GET("study/list")
     Call<CMRespDto<List<StudyListRespDto>>> getStudyList(@Query("phoneNumber") String phoneNumber,
                                                          @Query("interest") String interest,
                                                          @Query("lineup") String lineup);
+    @POST("study/create")
+    Call<CMRespDto<List<StudyListRespDto>>> createStudy();
+
+    @GET("study/detail")
+    Call<CMRespDto<List<StudyListRespDto>>> getStudyDetail();
+
 
 
 
